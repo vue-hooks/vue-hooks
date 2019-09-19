ad48b12dc17afe3fdeca85c89430ee385fdaf527
Yasin Uslu <nepjua@gmail.com>
2019-09-19 17:16:07 +0200

    docs: add basic-example

---
+++ examples/basic-example/src/components/MousePosition.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mouse-position">
+    <div class="position-x">X: {{ x }}</div>
+    <div class="position-y">Y: {{ y }}</div>
+  </div>
+</template>
+
+<script>
+import { useMousePosition } from 'vue-fn-use-mouse-position';
+
+export default {
+  setup() {
+    const [x, y] = useMousePosition();
+
+    return { x, y };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
