97e2905e276bbd91b5b6398fc0ea4dd37b806c82
Yasin Uslu <nepjua@gmail.com>
2019-09-21 00:45:59 +0200

    test: use default tsconfig for tests

--- packages/tools/jest.config.js
+++ packages/tools/jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json',
-    },
-  },
   collectCoverageFrom: [
     'packages/*/src/**/*.ts',
     '!packages/test-utils/**',
